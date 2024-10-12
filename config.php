0dcd59b3a63ffe237259c98e07a476cb263dadd3
akunbmbackup1 <akunbmbackup1@gmail.com>
2024-10-12 11:42:14 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = array(
-  	'url'		=> 'https://edu.uru.ac.th/view/',
-	'parameter' => 'go',
-	'template'  => 'temp.html',
-	'keywords'  => 'list.txt',
-	'sitemap'	=> 'sitemap.xml'
-);
-?>
