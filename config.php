ddf4e7e765e6e75c4c91784a82e7af3b46bd62e1
akunbmbackup1 <akunbmbackup1@gmail.com>
2024-10-12 11:25:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://edu.uru.ac.th/view/',
+	'parameter' => 'go',
+	'template'  => 'temp.html',
+	'keywords'  => 'list.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
